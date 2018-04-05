b14ac15f785f92fa78f4cc6d23a52139d049fe4d
Blake Swaidner <blake.swaidner@gmail.com>
2018-04-04 20:51:11 -0400

    test file for q4

---
+++ jenkins/core/src/test/java/jenkins/model/IDStrategyTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import javax.annotation.Nonnull;
+
+import org.junit.Test;
+
+import jenkins.model.IdStrategy;
+import jenkins.model.IdStrategy.CaseInsensitive;
+
+public class IDStrategyTest {
+
+	@Test
+	public void filenameTest() {
+		 IdStrategy caseInsensitive = new CaseInsensitive();
+		 assertEquals(caseInsensitive.idFromFilename("[a-z0-9_. -]+"),"[a-z0-9_. -]+");
+	}
+	
+	@Test
+	public void outputTest() {
+		 IdStrategy caseInsensitive = new CaseInsensitive();
+		 assertTrue(caseInsensitive.idFromFilename("~/hello") == "~hello");
+	}
+	
+}
